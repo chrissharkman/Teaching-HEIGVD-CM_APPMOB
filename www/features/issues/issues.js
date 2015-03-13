125a9c3198b6a7174d9c4dbea473bbbae663bef9
chrissharkman <ch.sharkman@gmail.com>
2015-03-13 23:41:05 +0100

    Fixing bug placeholder Image in production environnement v2

--- www/features/issues/issues.js
+++ www/features/issues/issues.js
@@ -8,7 +8,7 @@ angular.module('inspctr.issues', [])
 			$log.debug(error)
 		} else {
 			$scope.issues = data;
-			if (placeholderImage === 'true' && placeholder != 'false') {
+			if (placeholderImage === 'true' && placeholderImage !== 'false') {
 				$scope.issues.forEach(function(issue) {
 					issue.imageUrl = "img/placeholder.png";
 				})
